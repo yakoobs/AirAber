c0560d13bbb5cd46bd9a4fdc56ec1b2051b3b499
Kuba Sokolowski <jakub.m.sokolowski@gmail.com>
2017-07-10 10:53:12 +0200

    Prepare FlightInterfaceController

---
+++ Watch Extension/FlightInterfaceController.swift
@@ -0,0 +1,48 @@
+//
+//  FlightInterfaceController.swift
+//  AirAber
+//
+//  Created by Kuba Sokolowski on 10/07/2017.
+//  Copyright © 2017 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class FlightInterfaceController: WKInterfaceController {
+  @IBOutlet var flightLabel: WKInterfaceLabel!
+  @IBOutlet var routeLabel: WKInterfaceLabel!
+  @IBOutlet var boardingLabel: WKInterfaceLabel!
+  @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+  @IBOutlet var statusLabel: WKInterfaceLabel!
+  @IBOutlet var gateLabel: WKInterfaceLabel!
+  @IBOutlet var seatLabel: WKInterfaceLabel!
+
+  var flight: Flight? {
+    didSet {
+
+      guard let flight = flight else { return }
+
+      flightLabel.setText("Flight \(flight.shortNumber)")
+      routeLabel.setText(flight.route)
+      boardingLabel.setText("\(flight.number) Boards")
+      boardTimeLabel.setText(flight.boardsAt)
+
+      if flight.onSchedule {
+        statusLabel.setText("On Time")
+      } else {
+        statusLabel.setText("Delayed")
+        statusLabel.setTextColor(.red)
+      }
+      gateLabel.setText("Gate \(flight.gate)")
+      seatLabel.setText("Seat \(flight.seat)")
+    }
+  }
+
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+    
+    flight = Flight.allFlights().first
+  }
+}
